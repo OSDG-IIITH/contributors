2fa783b17ecfd5762a237f73636dc15758c1ba94
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2015-08-01 15:13:14 +0530

    Added extra features
    In case a repo is not labelled,
    User will be rated on the basis additions,deletions and commits

--- ranker/labelled_ranker.py
+++ ranker/labelled_ranker.py
@@ -3,15 +3,23 @@ import simplejson
 import json
 
 filters=['easy','medium','hard','bug']
-scoring=dict(zip(filters,[20,50,100,10]))
+stats=['additions','deletions','commits']
+scoring=dict(zip(filters+stats,[20,50,100,10]+[1,1,5]))
 users={}
+def init_user(login):
+    users[login]=dict(zip(filters+stats,len(filters+stats)*[0]))
+
 def req(get,params={}):
     """Function to send HTTP Requests"""
-    params['access_token']='da7992131c669b19e38da47fadf63e080174ffb7'
+    params['access_token']='888d06eaeca57ad57b8ac1b0fb96d86a96d4b491'
     api_host="https://api.github.com"
     url=api_host+get
     resp = requests.get(url=url, params=params)
-    data = json.loads(resp.text)
+
+    try:
+        data = json.loads(resp.text)
+    except:
+        data=[]
     return data
 
 
@@ -19,12 +27,10 @@ def get_repos(org):
     """Returns Repos under an organization"""
     url='/orgs/'+org+'/repos'
     repo_list=req(url)
-
     repos=[]
     for repo in repo_list:
         repos.append(repo["name"])
-
-    return  repos
+    return repos
 
 def get_repo_issues(repo,org,closed=True):
     """Returns a list of issues(closed issues, by default) under a repository"""
@@ -48,12 +54,12 @@ def filter_issues(issue_list):
 
     return filt_issues
 
-def update_count(issue_list):
+def update_issue_count(issue_list):
     for issue in issue_list:
         if(issue['assignee']):
             assignee=issue['assignee']['login']
             if not users.has_key(assignee):
-               users[assignee]=dict(zip(filters,len(filters)*[0]))
+                init_user(assignee)
         else:
             #no points if no assignee
             continue
@@ -64,16 +70,29 @@ def update_count(issue_list):
                 users[assignee][lname]+=1
 
 
-def score(lblct):
+def score(count):
 
     #defaut score is 10
     ans=10
 
 
-    for lbl,ct in lblct.iteritems():
+    for lbl,ct in count.iteritems():
             ans+=scoring[lbl]*ct
 
     return ans
+
+def update_contributors(repo,org):
+    """Updates list of contributors with additions deletions, and commit counts"""
+    url='/repos/'+org+'/'+repo+'/stats/contributors'
+    contribs=req(url)
+    for contrib in contribs:
+            login=contrib['author']['login']
+            if not users.has_key(login):
+               init_user(login)
+            for week in contrib['weeks']:
+                users[login]['additions']+=week['a']
+                users[login]['deletions']+=week['d']
+                users[login]['commits']+=week['c']
 
 
 
@@ -82,13 +101,14 @@ if __name__ == "__main__":
     filename="ranking.json"
     repos=get_repos(org)
     for repo in repos:
+        update_contributors(repo,org)
         issues=get_repo_issues(repo,org)
         issues=filter_issues(issues)
-        update_count(issues)
+        update_issue_count(issues)
 
     for user in users.iterkeys():
         total=score(users[user])
-        users[user][total]=total
+        users[user]['total']=total
 
     with open(filename, "w") as jsonFile:
             jsonFile.write(json.dumps(users))
